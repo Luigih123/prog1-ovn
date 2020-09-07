a366c0864f2b1e7c529b6fdadaec774f645269b2
Luigih123 <luigi.hansson@elev.ga.ntig.se>
2020-09-07 14:13:39 +0200

    Create Upg2.2.py

---
+++ Upg2.2.py
@@ -0,0 +1,8 @@
+stenar = 2
+print('£', stenar * 2e2)
+print('$', stenar * stenar)
+svar = input('skriv ett heltal: ')
+x = int(svar)
+y = x * x
+print('talet i kvadrat är', y)
+print(f'talet i kvadrat är {y: .2f}')

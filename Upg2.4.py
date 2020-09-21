5d5ef4a90ff70be142b4cfb33a5f0ced4945113f
Luigih123 <luigi.hansson@elev.ga.ntig.se>
2020-09-21 13:10:01 +0200

    Create Upg2.4.py

---
+++ Upg2.4.py
@@ -0,0 +1,8 @@
+svar = input('Omkrets räkning skriv ett tal')
+sida = float(svar)
+y = sida * 4
+print(f'Omkrets:{y:6}')
+svar = input('Area räkning skriv ett tal')
+sida = float(svar)
+y = sida * sida
+print(f'Area:{y:6}')
